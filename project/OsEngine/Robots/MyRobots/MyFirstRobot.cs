10ffafa7fdc5d9cca151760a81d3aa15816665c1
denchix <bubalo2000@mail.ru>
2025-01-23 19:55:13 +0300

    init

---
+++ project/OsEngine/Robots/MyRobots/MyFirstRobot.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OsEngine.Entity;
+using OsEngine.OsTrader.Panels;
+using OsEngine.OsTrader.Panels.Attributes;
+
+namespace OsEngine.Robots.MyRobots
+{
+    [Bot("MyFirstRobot")]
+    public class MyFirstRobot : BotPanel
+    {
+        public MyFirstRobot(string name, StartProgram program) : base(name, program)
+        {
+            
+        }
+
+        public override string GetNameStrategyType()
+        {
+            return "MyFirstRobot";
+        }
+
+        public override void ShowIndividualSettingsDialog()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
